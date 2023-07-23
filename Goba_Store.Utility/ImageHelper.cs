2799f78c51130724a385aa477bc97f0c91053fbf
Mohamed  Mahgoub <85798241+momahgoub172@users.noreply.github.com>
2023-07-23 15:46:21 +0300

    Extracting Image Functionalites (Upload , Delete) from controller

---
+++ Goba_Store.Utility/ImageHelper.cs
@@ -0,0 +1,43 @@
+﻿using Goba_Store.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Goba_Store.Utility
+{
+    public static class ImageHelper
+    {
+        public static string UploadImage(IWebHostEnvironment environment, IFormFileCollection files)
+        {
+            var rootPath = environment.WebRootPath;
+            var uploadedFile = rootPath + Constants.ImagePath;
+            var fileName = Guid.NewGuid().ToString();
+            var extention = Path.GetExtension(files[0].FileName);
+            //create new one and save it
+            using (FileStream stream = new FileStream(Path.Combine(uploadedFile, fileName + extention), FileMode.Create))
+            {
+                files[0].CopyTo(stream);
+            }
+
+            return fileName + extention;
+        }
+
+
+        public  static void DeleteImage(IWebHostEnvironment environment , Product product)
+        {
+            var rootPath = environment.WebRootPath;
+            var uploadedFile = rootPath + Constants.ImagePath;
+            var extention = Path.GetExtension(product.Image);
+            //delete old image
+            var oldImage = Path.Combine(uploadedFile, product.Image + extention);
+            if (File.Exists(oldImage))
+            {
+                File.Delete(oldImage);
+            }
+        }
+    }
+}
